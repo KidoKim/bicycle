477b708338eac7dd57c1ef6d5251b9db789c5690
park <szo12151@gmail.com>
2015-04-05 15:27:00 +0900

    init project

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+install_requires = [
+    'flask==0.10.1',
+    'sqlalchemy==0.9.9',
+    'alembic==0.7.4',
+    'gunicorn==19.3.0',
+    'fabric==1.10.1',
+]
+
+setup(
+    name='bicycle',
+    version='0.1',
+    install_requires=install_requires,
+)
